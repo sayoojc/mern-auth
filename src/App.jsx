ab2b3cf23ff5ad8d688a47fc25ba60d28bd1464c
sayooj c <sayoojcpalad@gmail.com>
2025-01-04 18:50:33 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Signup from './pages/SignUp'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+  <BrowserRouter>
+  
+  <Routes>
+    <Route path = "/" element = {<Home/>}/>
+    <Route path = "/about" element = {<About/>}/>
+    <Route path = "/sign-in" element = {<Signin/>}/>
+    <Route path = "/sign-up" element = {<Signup/>}/>
+    <Route path = "/profile" element = {<Profile/>} />
+
+  </Routes>
+  </BrowserRouter>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-blue-500 text-white p4 rounded'>Home</div>
+  )
+}
